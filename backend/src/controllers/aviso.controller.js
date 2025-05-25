cecabd47671ab650f26be11f99dc185e1c6623ec
Sebastian Albornoz <sebastian.albornoz2201@alumnos.ubiobio.cl>
2025-05-25 18:00:07 -0400

    Add funcion modificar avisos, pero sin validaciones

--- backend/src/controllers/aviso.controller.js
+++ backend/src/controllers/aviso.controller.js
@@ -1,7 +1,8 @@
 "use strict";
 import { 
     crearAvisoService,
-    obtenerAvisosService
+    obtenerAvisosService,
+    modificarAvisoService
 } from "../services/aviso.service.js";
 import {
     handleErrorClient,
@@ -39,6 +40,39 @@ export async function obtenerAvisosController(req, res){
         handleErrorServer(res, 500, error.message);
     }
 }
+
+export async function modificarAvisoController(req, res) {
+    try {
+        const { id } = req.params;
+         console.log("ID recibido en params:", id);
+        const { descripcion, categoria, fecha } = req.body;
+
+        if (!id) {
+            return handleErrorClient(res, 400, "Falta el parámetro obligatorio: id");
+        }
+        
+        if (!descripcion && !categoria && !fecha) {
+            return handleErrorClient(res, 400, "Debe proporcionar al menos un campo para actualizar: descripcion, categoria o fecha");
+        }
+
+        const camposActualizar = {};
+        if (descripcion !== undefined) camposActualizar.descripcion = descripcion;
+        if (categoria !== undefined) camposActualizar.categoria = categoria;
+        if (fecha !== undefined) camposActualizar.fecha = fecha;
+
+        const idNum = Number(id);
+        const [aviso, error] = await modificarAvisoService(idNum, camposActualizar);
+
+        if (error) {
+            return handleErrorClient(res, 400, error);
+        }
+
+        return handleSuccess(res, 200, "Aviso modificado exitosamente", aviso);
+    } catch (error) {
+        return handleErrorServer(res, 500, error.message);
+    }
+}
+
 
 
 
--- backend/src/services/aviso.service.js
+++ backend/src/services/aviso.service.js
@@ -34,3 +34,21 @@ export async function obtenerAvisosService(){
         return [null, "Error interno en el servidor"];
     }
 }
+
+export async function modificarAvisoService(id, camposActualizar) {
+    try {
+        const avisoRepository = AppDataSource.getRepository(Aviso);
+
+        const avisoFound = await avisoRepository.findOne({
+            where: { id: id },
+        });
+
+        if (!avisoFound) return [null, "No se encontró el aviso"];
+
+        await avisoRepository.update(id, camposActualizar);
+        return [await avisoRepository.findOne({ where: { id: id } }), null];
+    } catch (error) {
+        console.error("Error al actualizar el aviso", error);
+        return [null, "Error interno del servidor"];
+    }
+}
