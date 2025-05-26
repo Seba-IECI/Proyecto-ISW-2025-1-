41403d2db95dae9c14c66263a1ef97af328d9537
Héctor Díaz <164436353+hector280703@users.noreply.github.com>
2025-05-25 21:35:55 -0400

    Update index.routes.js

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -4,10 +4,7 @@ import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 import asambleaRoutes from "./asamblea.routes.js";
 import avisoRoutes from "./aviso.routes.js";
-<<<<<<< HEAD
 import emailRoutes from "./email.routes.js";
-=======
->>>>>>> eabc937a29f1101dedfa03e1ef659652dc58330e
 
 const router = Router();
 
@@ -15,11 +12,7 @@ router
     .use("/auth", authRoutes)
     .use("/user", userRoutes)
     .use("/aviso", avisoRoutes)
-<<<<<<< HEAD
     .use("/asamblea", asambleaRoutes)
     .use("/email", emailRoutes);
-=======
-    .use("/asamblea", asambleaRoutes);
->>>>>>> eabc937a29f1101dedfa03e1ef659652dc58330e
 
 export default router;
