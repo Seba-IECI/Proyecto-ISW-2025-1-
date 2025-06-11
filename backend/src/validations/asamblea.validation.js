f4ff185a95c0b6dc78662cee067535a1ebe675cf
Hector Diaz <hector.diaz2201@alumnos.ubiobio.cl>
2025-06-10 22:07:47 -0400

    Añado validaciones para actualizar asamblea

--- backend/src/validations/asamblea.validation.js
+++ backend/src/validations/asamblea.validation.js
@@ -32,3 +32,24 @@ export const asambleaQueryValidation = Joi.object({
         "date.min": "La fecha debe ser una fecha futura"
     })
 });
+
+export const asambleaUpdateValidation = Joi.object({
+    lugar: Joi.string()
+    .required()
+    .min(3)
+    .max(90)
+    .pattern(/^[a-zA-Z0-9\s]+$/)
+    .messages({
+        "string.empty": "El lugar es obligatorio", 
+        "string.min": "El lugar debe tener al menos 3 caracteres",
+        "string.max": "El lugar no puede exceder los 90 caracteres",
+        "string.pattern.base": "El lugar solo puede contener letras, números y espacios"
+    }),
+    fecha: Joi.date()
+    .required()
+    .min("now")
+    .messages({
+        "data.empty": "La fecha es obligatoria",
+        "date.min": "La fecha debe ser una fecha futura"
+    })
+});
